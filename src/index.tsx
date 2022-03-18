71942a29ff5354a9a7ce7d5889cdbbd5c7a45b58
Will-Andrade <willian.santos.andra@outlook.com>
2022-03-17 20:43:20 -0400

    feat: creates dummy data in miragejs using "seeds"

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-02-23 12:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Viver',
+          amount: 400,
+          createdAt: new Date('2022-02-26 07:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
